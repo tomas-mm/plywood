246cfe0c8dc4b58a5c3118bc14d884b65fbddba0
Tomas Montserrat <tomas.montserrat@smadex.com>
2018-11-14 18:43:28 +0100

    [MINOR]: Fix unlimited queue length in plywood concurrent requester (limit by enqueued time)

--- src/helper/concurrentLimitRequester.ts
+++ src/helper/concurrentLimitRequester.ts
@@ -1,33 +1,72 @@
+// the dealer has too many dependencies for browserify final build step
+var StatsD = require('lynx');
+var metrics = new StatsD('localhost', 8125, {
+  scope: 'production.pivot.dummy.',
+  on_error: () => {}
+});
+
 module Plywood {
   export module helper {
     export interface ConcurrentLimitRequesterParameters<T> {
       requester: Requester.PlywoodRequester<T>;
       concurrentLimit: int;
+      delayLimit?: int;
     }
 
     interface QueueItem<T> {
       request: Requester.DatabaseRequest<T>;
       deferred: Q.Deferred<any>;
+      deferTime: int;
     }
 
     export function concurrentLimitRequesterFactory<T>(parameters: ConcurrentLimitRequesterParameters<T>): Requester.PlywoodRequester<T> {
       var requester = parameters.requester;
       var concurrentLimit = parameters.concurrentLimit || 5;
+      var delayLimit = parameters.delayLimit || 60000;
 
       if (typeof concurrentLimit !== "number") throw new TypeError("concurrentLimit should be a number");
+      if (typeof delayLimit !== "number") throw new TypeError("delayLimit should be a number");
 
       var requestQueue: Array<QueueItem<T>> = [];
       var outstandingRequests: int = 0;
 
       function requestFinished(): void {
+        try {
+          metrics.gauge('plywood.queued.requests', requestQueue.length, 1);
+        } catch (e) {
+          console.log('ERROR: Failed sending statsd metric: ' + e);
+        }
+
         outstandingRequests--;
         if (!(requestQueue.length && outstandingRequests < concurrentLimit)) return;
-        var queueItem = requestQueue.shift();
-        var deferred = queueItem.deferred;
-        outstandingRequests++;
-        requester(queueItem.request)
-          .then(deferred.resolve, deferred.reject)
-          .fin(requestFinished);
+
+        var now: int = Date.now();
+        var elapsed: int = 0;
+        var queueItem: QueueItem<T>;
+        var deferred: Q.Deferred<any>;
+        while (requestQueue.length) {
+            queueItem = requestQueue.shift();
+            elapsed = now - queueItem.deferTime;
+            deferred = queueItem.deferred;
+
+            try {
+              metrics.gauge('plywood.queued.time', elapsed, 1);
+            } catch (e) {
+              console.log('ERROR: Failed sending statsd metric: ' + e);
+            }
+
+            if (elapsed > delayLimit) {
+                var message =  `Request has been queued for ${elapsed} ms (limit is ${delayLimit}): forcing reject`;
+                console.log(`DEBUG: ${message}`);
+                deferred.reject(new Error(message));
+                continue;
+            }
+            outstandingRequests++;
+            requester(queueItem.request)
+                .then(deferred.resolve, deferred.reject)
+                .fin(requestFinished);
+            return;
+        }
       }
 
       return (request: Requester.DatabaseRequest<T>): Q.Promise<any> => {
@@ -38,7 +77,8 @@ module Plywood {
           var deferred = Q.defer();
           requestQueue.push({
             request: request,
-            deferred: deferred
+            deferred: deferred,
+            deferTime: Date.now()
           });
           return deferred.promise;
         }
--- test/helper/concurrentLimitRequester.mocha.js
+++ test/helper/concurrentLimitRequester.mocha.js
@@ -1,4 +1,5 @@
 var { expect } = require("chai");
+var sinon = require('sinon');
 
 var Q = require('q');
 
@@ -6,6 +7,7 @@ var plywood = require("../../build/plywood");
 var { concurrentLimitRequesterFactory } = plywood.helper;
 
 describe("Retry requester", () => {
+  var sandbox = sinon.createSandbox();
   var makeRequester = () => {
     var deferreds = {};
 
@@ -26,6 +28,10 @@ describe("Retry requester", () => {
     };
     return requester;
   };
+
+  afterEach(function () {
+    sandbox.restore();
+  });
 
   it("basic works", (testComplete) => {
     var requester = makeRequester();
@@ -81,6 +87,71 @@ describe("Retry requester", () => {
     expect(requester.hasQuery('a'), 'has a').to.equal(true);
     expect(requester.hasQuery('b'), 'has b').to.equal(true);
     expect(requester.hasQuery('c'), 'has c').to.equal(false);
+    requester.resolve('a');
+    return requester.resolve('b');
+  });
+
+  it("limit delay works", (testComplete) => {
+    var requester = makeRequester();
+    var concurrentLimitRequester = concurrentLimitRequesterFactory({
+      requester,
+      concurrentLimit: 2,
+      delayLimit: 5,
+    });
+
+    var mockDate = sandbox.mock(Date);
+    var expectationDate = mockDate.expects('now').exactly(4)
+      .onCall(0).returns(0)
+      .onCall(1).returns(1)
+      .onCall(2).returns(2)
+      .onCall(3).returns(20);
+
+    var nextQuery = 'a';
+    concurrentLimitRequester({ query: 'a' })
+      .then((res) => {
+        expect(res).to.be.an('array');
+        expect(nextQuery).to.equal('a');
+        return nextQuery = 'b';
+      })
+      .done();
+
+    concurrentLimitRequester({ query: 'b' })
+      .then((res) => {
+        expect(res).to.be.an('array');
+        expect(nextQuery).to.equal('b');
+        nextQuery = 'c';
+        expect(requester.hasQuery('c', 'has c')).to.equal(true);
+        return requester.resolve('c');
+      })
+      .done();
+
+    concurrentLimitRequester({ query: 'c' })
+      .then((res) => {
+        expect(res).to.be.an('array');
+        expect(nextQuery).to.equal('c');
+        expect(requester.hasQuery('d', 'has d')).to.equal(false);
+        try {
+          sandbox.verify();
+          testComplete();
+        } catch (err) {
+          testComplete(err);
+        }
+      })
+      .done();
+
+    concurrentLimitRequester({ query: 'd' })
+      .catch((rejectError) => {
+        // Assert that a request that timed out is rejected
+        expect(rejectError).to.be.an('Error');
+        // just after 'a' finished, 'd' is removed from the queue
+        expect(nextQuery).to.equal('a');
+      })
+      .done();
+
+    expect(requester.hasQuery('a'), 'has a').to.equal(true);
+    expect(requester.hasQuery('b'), 'has b').to.equal(true);
+    expect(requester.hasQuery('c'), 'has c').to.equal(false);
+    expect(requester.hasQuery('d'), 'has d').to.equal(false);
     requester.resolve('a');
     return requester.resolve('b');
   });
